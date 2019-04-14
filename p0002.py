40e49ee8576bfb188a9c6c1fca0f0bf29ae6591a
LucasGracia <LucasGracia@users.noreply.github.com>
2019-04-14 17:07:02 +0100

    solution to problem 2 added

---
+++ p0002.py
@@ -0,0 +1,48 @@
+"""
+Solution to Project Euler problem 2.
+    'By considering the terms in the Fibonacci sequence whose values
+     do not exceed four million, find the sum of the even-valued terms.'
+
+https://github.com/LucasGracia/Project-Euler
+"""
+__author__ = 'Lucas Gracia'
+__version__ = '1.0.0'
+__license__ = 'MIT'
+
+import time
+
+
+def sum_fibs(limit):
+    """Sums even Fibonacci numbers upto `limit`
+
+    Args:
+        limit (int) : The limit the sum will be calculated to
+
+    Returns:
+        int : The sum of even Fibonacci numbers upto `limit`
+    """
+    previous = 0
+    current = 1
+    future = 1
+    result = 0
+
+    while True:
+        future = current + previous
+        if future < limit:
+            if future % 2 == 0:
+                result += future
+        else:
+            break
+        previous = current
+        current = future
+
+    return result
+
+
+if __name__ == '__main__':
+    START_TIME = time.time()
+
+    ANSWER = sum_fibs(4000000)
+
+    END_TIME = time.time()
+    print("Answer:  {}\nTime: {}s".format(ANSWER, END_TIME - START_TIME))
