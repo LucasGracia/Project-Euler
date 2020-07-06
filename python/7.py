1b25b940f64c0d9373c49034da43009c7195f626
Lucas Gracia <lucas@Lucass-MacBook-Pro.local>
2020-07-06 11:23:15 +0100

    python soultion to problem 7 added

---
+++ python/7.py
@@ -0,0 +1,33 @@
+import time
+
+#Working Variables
+x = 15
+isPrimeBool = False
+primeList = [2,3,5,7,11,13]
+startTime = time.time()
+
+def isPrime(x):
+    isPrimeBool = False
+    i = 2
+    while i < x:
+        if x % i == 0:
+            return False
+        else:
+            i += 1
+    print("List length: ", len(primeList))
+    return True
+
+
+
+
+while len(primeList) != 10001:
+    isPrimeBool = isPrime(x)
+    if isPrimeBool:
+        #Add number to primeList
+        primeList.append(x)
+        x += 2
+    else:
+        x += 2
+
+print("Answer: ", primeList[10000])
+print("Running Time: ", time.time()-startTime, " seconds")
