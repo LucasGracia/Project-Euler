6766cf5cc95154aace68ce41abf8e74903f9c025
Lucas Gracia <lucas@Lucass-MacBook-Pro.local>
2020-07-06 11:24:14 +0100

    python solution to problem 14 added

---
+++ python/14.py
@@ -0,0 +1,33 @@
+import time
+
+#Variables
+start = time.time()
+workingLength = 0
+chainLength = 0
+chainStart = 0
+
+def collatzEven(number):
+    return number / 2
+
+def collatzOdd(number):
+    return (number * 3) + 1
+
+def collatzChain(number):
+    i = 0
+    while number != 1:
+        i += 1
+        if number % 2 == 0:
+            number = collatzEven(number)
+        else:
+            number = collatzOdd(number)
+    return i
+
+for i in range(2,1000000+1):
+    workingLength = collatzChain(i)
+    if workingLength > chainLength:
+        chainLength = workingLength
+        chainStart = i
+        print(i)
+
+print("Answer =", chainStart,", length =", chainLength)
+print("Time =", time.time()-start, "s")
