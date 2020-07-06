a92b4bc6ed67bd42d2ca6672e5134b9f5ea0cdf2
Lucas Gracia <lucas@lucass-mbp.home>
2020-07-06 11:37:48 +0100

    uplifted to pass pycodestyle

--- python/4.py
+++ python/4.py
@@ -4,19 +4,19 @@ two 3 digit numbers
 """
 import time
 
-#Declare Variables
 start = time.time()
 palindrome = 0
 
-#Declare functions
+
 def isPalindrome(number):
     if str(number) == str(number)[::-1]:
         return True
     else:
         return False
 
-for x in range(1,999):
-    for y in range(1,999):
+
+for x in range(1, 999):
+    for y in range(1, 999):
         if isPalindrome(x*y):
             if x * y > palindrome:
                 palindrome = x * y
