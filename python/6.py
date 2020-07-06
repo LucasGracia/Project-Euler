b3b483f7759992ed8389c01802f92eec7716058b
Lucas Gracia <lucas@lucass-mbp.home>
2020-07-06 11:49:33 +0100

    uplifted to pass pycodestyle

--- python/6.py
+++ python/6.py
@@ -1,21 +1,26 @@
-#A program to find the difference between the sum of squares 1-100 and the square of sum 1-100
+"""
+Solution to Project Euler Problem 6
+"""
 import time
 
 startTime = time.time()
 
+
 def squareOfSum():
     total = 0
-    for i in range(1,101):
+    for i in range(1, 101):
         total += i
 
     return total ** 2
 
+
 def sumOfSquares():
     total = 0
-    for i in range (1,101):
+    for i in range(1, 101):
         total += (i ** 2)
 
     return total
+
 
 squareOfSum = squareOfSum()
 sumOfSquares = sumOfSquares()
