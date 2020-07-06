c05d8d34e95d9d884fa0c898589905c900b762b7
Lucas Gracia <lucas@Lucass-MacBook-Pro.local>
2020-07-06 11:23:47 +0100

    python soultion to problem 12 added

---
+++ python/12.py
@@ -0,0 +1,22 @@
+import time
+
+#Variables
+start = time.time()
+
+def factors(number):
+    factors = []
+    for i in range(1,number + 1):
+        if number % i == 0:
+            factors.append(i)
+    return factors
+
+triangularNumber = 0
+for i in range(1,100000):
+    triangularNumber += i
+    if len(factors(triangularNumber)) > 500:
+        print ("Answer =", triangularNumber)
+        break
+    else:
+        print (i)
+                    
+print("Time =", time.time()-start, "s")
